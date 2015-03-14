d05e97fb3d7fd06930f7c83677c357120177a27f
kernel-io <dave@progressivegangster.com>
2015-03-15 02:41:56 +1100

    Update active_record.rb
    this is what happens when you use the github code editor instead of cloning locally..

--- lib/validates_timeliness/orm/active_record.rb
+++ lib/validates_timeliness/orm/active_record.rb
@@ -23,9 +23,9 @@ module ValidatesTimeliness
         end
         
         def lookup_cast_type(sql_type)
-          ::ActiveRecord::Type::DateTime if type == :datetime
-          ::ActiveRecord::Type::Date if type == :date
-          ::ActiveRecord::Type::Time if type == :time
+          ::ActiveRecord::Type::DateTime if sql_type == :datetime
+          ::ActiveRecord::Type::Date if sql_type == :date
+          ::ActiveRecord::Type::Time if sql_type == :time
         end
       
         def define_attribute_methods
