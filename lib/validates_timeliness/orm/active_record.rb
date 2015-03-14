a1d6ccd964609cf2035617913cea65cdaaf12e36
kernel-io <dave@progressivegangster.com>
2015-03-15 02:37:22 +1100

    Update active_record.rb
    this should work now.

--- lib/validates_timeliness/orm/active_record.rb
+++ lib/validates_timeliness/orm/active_record.rb
@@ -17,10 +17,17 @@ module ValidatesTimeliness
         def timeliness_column_for_attribute(attr_name)
           columns_hash.fetch(attr_name.to_s) do |attr_name|
             validation_type = _validators[attr_name.to_sym].find {|v| v.kind == :timeliness }.type
-            ::ActiveRecord::ConnectionAdapters::Column.new(attr_name, nil,  ::ActiveRecord::Type::Value.new, validation_type.to_s)
+            cast_type = lookup_cast_type(validation_type)
+            ::ActiveRecord::ConnectionAdapters::Column.new(attr_name, nil, cast_type)
           end
         end
-
+        
+        def lookup_cast_type(sql_type)
+          ::ActiveRecord::Type::DateTime if type == :datetime
+          ::ActiveRecord::Type::Date if type == :date
+          ::ActiveRecord::Type::Time if type == :time
+        end
+      
         def define_attribute_methods
           super.tap do |attribute_methods_generated|
             define_timeliness_methods true
